5a24f3934486f62dd78f8632118afa57e0cb7377
cranucci <cranucci@sissa.it>
2025-05-28 08:36:49 -0700

    templates now downloaded from webpage; nmt binning is now hard-coded; MCMs computation moved here and updated

--- scripts/templates_fit.py
+++ scripts/templates_fit.py
@@ -1,18 +1,19 @@
 import yaml
 import argparse
 import os
+import wget
 import numpy as np
 import healpy as hp
 import pymaster as nmt
 import utils as u
-from scipy.optimize import curve_fit
 
 
 def templates_fit(args):
     """
     Get power law spectral parameters by fitting PySM foregrounds templates.
-    Read dust and synchrotron templates, downgrade, rotate and mask;
-    then compute power spectra and fit parameters (amplitude and index).
+    Read dust and synchrotron templates, downgrade, and rotate;
+    then evaluate power spectra on a mask and fit foregrounds
+    spectral parameters (amplitude and index).
     -----------------
     Inputs:
         args: yaml configuration file with parameters
@@ -27,12 +28,12 @@ def templates_fit(args):
         config = yaml.load(f, Loader=yaml.FullLoader)
 
     out_dir = config['output_dir']
-    plots_dir = out_dir + "/plots"
-    alms_dir = out_dir + "/alm_templates"
-    mcms_dir = out_dir + "/mcms"
+    plots_dir = f"{out_dir}/plots"
+    templates_dir = f"{out_dir}/templates"
+    mcms_dir = f"{out_dir}/mcms"
 
     # create directories
-    dirs = [out_dir, plots_dir, alms_dir, mcms_dir]
+    dirs = [out_dir, plots_dir, templates_dir, mcms_dir]
     for d in dirs:
         os.makedirs(d, exist_ok=True)
 
@@ -45,72 +46,67 @@ def templates_fit(args):
     global ell_0
     ell_0 = int(config['plaw_ell_pivot'])
 
-    # templates directories
-    pysm_dir = "/global/cfs/cdirs/cmb/www/pysm-data"
-    dust_dir = f"{pysm_dir}/dust_gnilc"
-    synch_dir = f"{pysm_dir}/synch"
-
     print(f" - nside = {nside_out} -> lmax = {lmax_out}")
     print(f" - ell_pivot = {ell_0}")
 
-    fname_synch = f"{synch_dir}/synch_template_nside{nside_in}_2023.02.25.fits"  # noqa
-    fname_dust = f"{dust_dir}/gnilc_dust_template_nside{nside_in}_2023.02.10.fits"  # noqa
+    # get and pre-process synchrotron and dust templates
     ref_freqs = [23., 353.]  # synch, dust
+    pysm_url = "https://portal.nersc.gov/cfs/cmb/pysm-data"
+    url_synch = f"{pysm_url}/synch/synch_template_nside{nside_in}_2023.02.25.fits"
+    url_dust = f"{pysm_url}/dust_gnilc/gnilc_dust_template_nside{nside_in}_2023.02.10.fits"
+    fname_synch = f"{templates_dir}/template_synch_maps.fits"
+    fname_dust = f"{templates_dir}/template_dust_maps.fits"
+    fname_alm_synch = f"{templates_dir}/template_synch_alm_lmax{lmax_in}.fits"
+    fname_alm_dust = f"{templates_dir}/template_dust_alm_lmax{lmax_in}.fits"
 
-    # read IQU; units: uK_RJ
-    print("-------------------")
-    print("reading templates")
-    synch_rj = hp.read_map(fname_synch, field=[0, 1, 2])
-    dust_rj = hp.read_map(fname_dust, field=[0, 1, 2])
+    # if alm do not exist in the out directory
+    if not os.path.isfile(fname_alm_synch) or not os.path.isfile(fname_alm_dust):
+        print("-------------------")
+        print("computing templates alm")
+        # download templates if not present
+        if not os.path.isfile(fname_synch) or not os.path.isfile(fname_dust):
+            print("downloading templates")
+            wget.download(url_synch, fname_synch)
+            wget.download(url_dust, fname_dust)
 
-    rj2cmb = u.Krj_Kcmb(ref_freqs)
-    synch_cmb = rj2cmb[0] * synch_rj
-    dust_cmb = rj2cmb[1] * dust_rj
+        # read IQU; units: uK_RJ
+        print("-------------------")
+        print("reading templates")
+        synch_rj = hp.read_map(fname_synch, field=[0, 1, 2])
+        dust_rj = hp.read_map(fname_dust, field=[0, 1, 2])
 
-    # free some space
-    del synch_rj
-    del dust_rj
+        rj2cmb = u.Krj_Kcmb(ref_freqs)
+        synch_cmb = rj2cmb[0] * synch_rj
+        dust_cmb = rj2cmb[1] * dust_rj
 
-    # downgrade, rotate
-    print("-------------------")
-    print("computing templates alm")
-    fname_alm_synch = f"{alms_dir}/alm_synch_template_nside{nside_in}.npz"
-    fname_alm_dust = f"{alms_dir}/alm_dust_template_nside{nside_in}.npz"
-    print("- synchro")
-    if not os.path.isfile(fname_alm_synch):
+        # free some space
+        del synch_rj
+        del dust_rj
+
+        # downgrade, rotate
+        print("- synchro")
         alm_synch = hp.map2alm(synch_cmb, use_pixel_weights=True)
-        np.savez(fname_alm_synch, alm_T=alm_synch[0], alm_E=alm_synch[1],
-                 alm_B=alm_synch[2])
-    else:
-        print("  loading")
-        alm_file = np.load(fname_alm_synch)
-        alm_synch = np.array([alm_file['alm_T'], alm_file['alm_E'],
-                              alm_file['alm_B']])
-    print("- dust")
-    if not os.path.isfile(fname_alm_dust):
-        alm_dust = hp.map2alm(dust_cmb, use_pixel_weights=True)
-        np.savez(fname_alm_dust, alm_T=alm_dust[0], alm_E=alm_dust[1],
-                 alm_B=alm_dust[2])
-    else:
-        print("  loading")
-        alm_file = np.load(fname_alm_dust)
-        alm_dust = np.array([alm_file['alm_T'], alm_file['alm_E'],
-                             alm_file['alm_B']])
+        # write alm to disk
+        hp.write_alm(fname_alm_synch, alm_synch, overwrite=True, out_dtype=np.float64)
 
-    if not args.plots:
+        print("- dust")
+        alm_dust = hp.map2alm(dust_cmb, use_pixel_weights=True)
+        hp.write_alm(fname_alm_dust, alm_dust, overwrite=True, out_dtype=np.float64)
+
         del synch_cmb
         del dust_cmb
+    else:
+        print("loading templates alm")
+        alm_synch = hp.read_alm(fname_alm_synch, hdu=(1, 2, 3))
+        alm_dust = hp.read_alm(fname_alm_dust, hdu=(1, 2, 3))
 
     print("-------------------")
-    print(f"clipping to lmax={lmax_out}")
-    # which alms indices to clip for downgrading
-    clip_indices = []
-    for m in range(lmax_out+1):
-        clip_indices.append(hp.Alm.getidx(lmax_in, np.arange(m, lmax_out+1), m))  # noqa
-    clip_indices = np.concatenate(clip_indices)
-    # clipping alms at l>lmax_out to avoid artifacts
-    alm_clip_synch = [each[clip_indices] for each in alm_synch]
-    alm_clip_dust = [each[clip_indices] for each in alm_dust]
+    print(f"clipping alm to lmax={lmax_out}")
+    alm_clip_synch = hp.resize_alm(alm_synch, lmax_in, lmax_in, lmax_out, lmax_out)
+    alm_clip_dust = hp.resize_alm(alm_dust, lmax_in, lmax_in, lmax_out, lmax_out)
+
+    maps_synch_dg = hp.alm2map(alm_clip_synch, nside=nside_out)
+    maps_dust_dg = hp.alm2map(alm_clip_dust, nside=nside_out)
 
     # free some space
     del alm_synch
@@ -122,29 +118,32 @@ def templates_fit(args):
     hp.rotate_alm(alm_clip_dust, *angles)
 
     print(f"reprojecting alm to maps at nside={nside_out}")
-    synch_maps = hp.alm2map(alm_clip_synch, nside=nside_out)
-    dust_maps = hp.alm2map(alm_clip_dust, nside=nside_out)
+    maps_synch = hp.alm2map(alm_clip_synch, nside=nside_out)
+    maps_dust = hp.alm2map(alm_clip_dust, nside=nside_out)
 
     mask = u.load_mask(config['mask'], nside_out)
 
     print("binning")
-    if config['bpw_edges'] is not None:
-        bpw_edges = np.loadtxt(config['bpw_edges']).astype(int)
-    else:
-        ell_per_bin = 4
-        bpw_edges = np.arange(2, lmax_out, ell_per_bin)
-        config['bpw_edges'] = f"bandpowers of constant width ({ell_per_bin} multipoles per bin)"  # noqa
-    bins = u.get_bins(nside_out, bpw_edges, dell=False)
+    delta_ell = 4
+    bins = nmt.NmtBin.from_lmax_linear(lmax_out, nlb=delta_ell)
     ell_arr = bins.get_effective_ells()
 
     print("mode coupling matrices")
-    workspaces = u.compute_mcms(mcms_dir, nside_out, bins, mask)
+    fname_mcm_0x0 = f"{mcms_dir}/mcm_0x0.fits"
+    fname_mcm_2x2 = f"{mcms_dir}/mcm_2x2.fits"
+    f0 = nmt.NmtField(mask, None, spin=0)
+    f2 = nmt.NmtField(mask, None, spin=2)
+    wsp_0x0 = nmt.NmtWorkspace.from_fields(f0, f0, bins)
+    wsp_2x2 = nmt.NmtWorkspace.from_fields(f2, f2, bins)
+    wsp_0x0.write_to(fname_mcm_0x0)
+    wsp_2x2.write_to(fname_mcm_2x2)
+    workspaces = [wsp_0x0, wsp_2x2]
 
     print("compute power spectra")
     print("- synchro")
-    cls_s = u.compute_cls(synch_maps, mask, workspaces)
+    cls_s = u.compute_cls(maps_synch, mask, workspaces)
     print("- dust")
-    cls_d = u.compute_cls(dust_maps, mask, workspaces)
+    cls_d = u.compute_cls(maps_dust, mask, workspaces)
 
     print("-------------------")
     print("fit power spectra with power law")
@@ -152,18 +151,11 @@ def templates_fit(args):
     A_dust, alpha_dust, fit_min_d, fit_max = u.fit_routine(ell_arr, cls_d)
 
     # update config file with fitted parameters
-    config['A_synch_TT'] = A_synch[0]
-    config['A_synch_EE'] = A_synch[1]
-    config['A_synch_BB'] = A_synch[2]
-    config['A_dust_TT'] = A_dust[0]
-    config['A_dust_EE'] = A_dust[1]
-    config['A_dust_BB'] = A_dust[2]
-    config['alpha_synch_TT'] = alpha_synch[0]
-    config['alpha_synch_EE'] = alpha_synch[1]
-    config['alpha_synch_BB'] = alpha_synch[2]
-    config['alpha_dust_TT'] = alpha_dust[0]
-    config['alpha_dust_EE'] = alpha_dust[1]
-    config['alpha_dust_BB'] = alpha_dust[2]
+    for i, pols in enumerate(['TT', 'EE', 'BB']):
+        config[f'A_synch_{pols}'] = A_synch[i]
+        config[f'A_dust_{pols}'] = A_dust[i]
+        config[f'alpha_synch_{pols}'] = alpha_synch[i]
+        config[f'alpha_dust_{pols}'] = alpha_dust[i]
 
     # Copy the configuration file to output directory
     fit_paramfile = args.globals.replace(".yaml", "_fit.yaml")
@@ -173,9 +165,8 @@ def templates_fit(args):
 
     if args.plots:
         print("-------------------")
-        print("plotting...")
-        u.plotter_templates(plots_dir, synch_cmb, dust_cmb,
-                            synch_maps, dust_maps,
+        u.plotter_templates(plots_dir, maps_synch_dg, maps_dust_dg,
+                            maps_synch, maps_dust,
                             mask, cls_s, cls_d,
                             A_synch, A_dust, alpha_synch, alpha_dust,
                             fit_min_s, fit_min_d, fit_max, ell_arr, ell_0)
@@ -183,7 +174,7 @@ def templates_fit(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate alm sims for transfer function")
+        description="Extract foregrounds spectral parameters in a sky region")
     parser.add_argument("--globals", type=str,
                         help="Path to the yaml with global parameters")
     parser.add_argument("--plots", action="store_true",
